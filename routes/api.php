5f125b943bdb60dfff7ec676eb100c6740d7bb65
Eugene Bednik <me@eugenebednik.com>
2025-10-23 11:54:47 -0700

    Add Rate Limiting

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ Route::prefix('api')->group(function () {
     Route::prefix('v1')->group(function () {
         Route::prefix('kills')->group(function () {
             Route::post('/', [KillsApiController::class, 'create'])
-                ->middleware(['api-key'])
+                ->middleware(['throttle:submit-kill', 'api-key'])
                 ->name('api.kills.create');
         });
     });
