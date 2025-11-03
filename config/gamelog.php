67a9849d24acc26778bcba98d57af4905f6ded18
Eugene Bednik <eugene.bednik@bisnow.com>
2025-11-03 02:02:21 -0800

    Pattern-based matching

--- config/gamelog.php
+++ config/gamelog.php
@@ -2,14 +2,14 @@
 
 return [
     'actor_kill_string' => env('ACTOR_KILL_STRING', 'Actor Death'),
+    'ac_match_string' => env('AC_MATCH_STRING', 'Requesting Mode Change'),
+    'actor_death_pattern' => env('ACTOR_DEATH_PATTERN', "/<(?<timestamp>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z)> \[Notice\] <Actor Death> CActor::Kill: '(?<victim>[^']+)' \[(?<victimId>\d+)\] in zone '(?<zone>[^']+)' killed by '(?<killer>[^']+)' \[(?<killerId>\d+)\] using '(?<weapon>[^']+)' \[Class (?<class>[^\]]+)\] with damage type '(?<damageType>[^']+)'/"),
+    'vehicle_destruction_pattern' => env('VEHICLE_DESTRUCTION_PATTERN', "/<Vehicle Destruction> CVehicle::OnAdvanceDestroyLevel: Vehicle '(?<vehicle>[^']+)' \[(?<vehicleId>\d+)\] in zone '(?<zone>[^']+)'(?:.*)caused by '[^']+' \[\d+\](?:.*)with 'Combat' \[Team_CGP4\]\[(?<team>[^\]]+)\]/"),
     'vehicle_destruction_string' => env('VEHICLE_DESTRUCTION_STRING', 'Vehicle Destruction'),
     'valid_pvp_damage_types' => [
         'VehicleDestruction',
         'Bullet',
-    ],
-    'iso_timestamp_pattern' => env('ISO_TIMESTAMP_PATTERN', '/\<(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|[+-]\d{2}:\d{2}))\>/'),
-    'arena_commander_zone_prefixes' => [
-        'ooc_',
+        'TakeDown',
     ],
     'self_destruct_weapon' => env('SELF_DESTRUCT_WEAPON', 'SelfDestruct'),
     'lines_to_read' => env('GAME_LOG_LINES_TO_READ', 4),
