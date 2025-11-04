9d96d5ead153ce092b4c4e6160d24ee535e32e7d
Eugene Bednik <eugene.bednik@bisnow.com>
2025-11-04 08:47:30 -0800

    Cookie consent fix

--- resources/views/livewire/cookie-consent.blade.php
+++ resources/views/livewire/cookie-consent.blade.php
@@ -13,18 +13,6 @@
     class="fixed inset-0 z-50 flex items-end justify-center p-4 sm:p-6 pointer-events-none"
     x-cloak
 >
-    <!-- Backdrop -->
-    <div
-        x-show="show"
-        x-transition:enter="transition ease-out duration-300"
-        x-transition:enter-start="opacity-0"
-        x-transition:enter-end="opacity-100"
-        x-transition:leave="transition ease-in duration-200"
-        x-transition:leave-start="opacity-100"
-        x-transition:leave-end="opacity-0"
-        class="fixed inset-0 bg-black bg-opacity-50 pointer-events-none"
-    ></div>
-
     <!-- Modal -->
     <div
         x-show="show"
